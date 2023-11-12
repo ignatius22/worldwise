0d2f50f28677e44870178b8f1d43d627c1ab7783
ignatius22 <igee54077@gmail.com>
2023-11-12 07:26:07 +0100

    Share state using useSearchParams and queryString

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
 import CityList from "./components/CityList";
 import { useEffect, useState } from "react";
+import CountryList from "./components/CountryList";
+import City from "./components/City";
 
 function App() {
   const [cities, setCities] = useState([]);
@@ -40,7 +42,8 @@ function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="cities/:id" element={<City/>} />
+          <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading} />} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,18 @@
-import styles from './Map.module.css'
+import { useSearchParams } from "react-router-dom";
+import styles from "./Map.module.css";
 
 function Map() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  let lat = searchParams.get("lat");
+  let lng = searchParams.get("lng");
   return (
-    <div className={styles.mapContainer}>Map</div>
-  )
+    <div className={styles.mapContainer}>
+      <h1>Map</h1>
+      <h1>
+        position: {lat},{lng}
+      </h1>
+    </div>
+  );
 }
 
-export default Map
+export default Map;
