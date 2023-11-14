784619ac8581ec48900ac61ed7a9db56a6e2ddaa
ignatius22 <igee54077@gmail.com>
2023-11-14 20:56:33 +0100

    Add login and logout feature

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CitiesProvider>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </CitiesProvider>
   </React.StrictMode>
 );
